51dfdbdfe68e5f923c04720061ec1f9df20989d0
1langit <langitlintangradjendra@mail.ugm.ac.id>
2023-09-28 15:44:31 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 //route resource
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
-Route::get('/posts/create', 'PostController@create')->name('posts.create');
